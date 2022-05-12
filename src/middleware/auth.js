abc2b022b392e010c063254d4aae4bc4e1026b18
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-05-12 02:27:53 -0500

    Minor Fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,6 @@
 const helpers = {
     isAuthenticated: (req, res, next) => {
-        if(!req.user) return res.redirect('/login');
+        if(!req.user) res.status(401).json({ msg: 'Not Authenticated' });
         return next();
     }
 };
