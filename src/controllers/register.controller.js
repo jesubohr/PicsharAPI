548a1438b410213534bef83bbf3eef5678f8259f
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-05-21 23:49:55 -0500

    Update on parameter, and responses props name

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -1,19 +1,19 @@
 const User = require('../models/User.model');
 
 async function RegisterUser (req, res) {
-    const { name, birthday, email, password } = req.body;
+    const { fullname: name, birthday, email, password } = req.body;
 
     if (!name || !birthday || !email || !password) {
-        return res.status(400).json({ msg: 'Please fill all the fields' });
+        return res.status(400).json({ error: 'Please fill all the fields' });
     }
 
     const user = await User.findOne({ email });
-    if (user) return res.status(400).json({ msg: 'User already exists' });
+    if (user) return res.status(400).json({ error: 'User already exists' });
 
     const newUser = new User({ name, birthday, email, password });
     newUser.password = await newUser.encryptPassword(password);
     await newUser.save();
-    res.json({ msg: 'User created successfully' });
+    res.json({ success: 'User created successfully' });
 }
 
 module.exports = { RegisterUser };
