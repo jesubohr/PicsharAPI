b1bc9b577ac1cec406bb5a9819dd123fb1a7da71
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-05-22 18:32:52 -0500

    Created tests that comply with requirements

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,8 +1,60 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+const createServer = require('../src/server');
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
 describe('Login', () => {
-    it.todo('valid login');
+    let server;
+    beforeAll(() => {
+        server = createServer();
+        mongoose.connect(MONGO_URI, {}, (err) => {
+            if (err) console.error(err);
+            else console.log('Connected to MongoDB');
+        });
+    });
 
-    it.todo('invalid login');
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
 
-    it.todo('invalid login with wrong password');
+    it('valid login', async () => {
+        const user = {
+            username: 'jameswo',
+            password: 'loquendo',
+        };
+        const res = await request(server)
+            .post('/users/login')
+            .send(user);
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toHaveProperty('token');
+    });
+
+    it('invalid login, wrong username', async () => {
+        const user = {
+            username: 'user123',
+            password: 'loquendo',
+        };
+        const res = await request(server)
+            .post('/users/login')
+            .send(user);
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty('error');
+        expect(res.body.error).toBe('User not found');
+    });
+
+    it('invalid login, wrong password', async () => {
+        const user = {
+            username: 'jameswo',
+            password: '123456',
+        };
+        const res = await request(server)
+            .post('/users/login')
+            .send(user);
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty('error');
+        expect(res.body.error).toBe('Incorrect password');
+    });
 });
 
--- tests/register.test.js
+++ tests/register.test.js
@@ -1,6 +1,52 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+const createServer = require('../src/server');
+const User = require('../src/models/User.model');
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
 describe('Register', () => {
-    it.todo('valid register');
+    let server;
+    beforeAll(() => {
+        server = createServer();
+        mongoose.connect(MONGO_URI, {}, (err) => {
+            if (err) console.error(err);
+            else console.log('Connected to MongoDB');
+        });
+    });
 
-    it.todo('invalid register');
+    afterAll(async () => {
+        await User.deleteMany();
+        await mongoose.connection.close();
+    });
 
+    it('valid register', async () => {
+        const user = {
+            username: "elonmusk",
+            password: "iamveryrich",
+            email: "elon@musk.com",
+            birthdate: "06/28/1971",
+            bio: "I am Elon Musk"
+        };
+        const res = await request(server)
+            .post('/users/')
+            .send(user);
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toHaveProperty('token');
+    });
+
+    it('invalid register', async () => {
+        const user = {
+            username: "elonmusk",
+            password: "iamveryrich",
+            email: "elon@musk.com",
+        };
+        const res = await request(server)
+            .post('/users/')
+            .send(user);
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty('error');
+        expect(res.body.error).toBe('Please fill all the fields');
+    });
 });
