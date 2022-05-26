7ceb2bcde21d4c977c2e90e050825d68155b70e3
Valery Retamoza <vretamoza@uninorte.edu.co>
2022-05-25 22:52:10 -0500

    Correct some mistakes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,9 @@
-const User = require('../models/user.model');
+const User = require('../models/User.model');
 
 async function UserInfo (req, res) {
     const { user_id } = req.query;
     try {
-        const user = await User.findByuser_id(user_id).select('-password -birthday');
+        const user = await User.findById(user_id).select('-password -birthdate');
         res.json({ user });
     } catch (error) {
         res.status(500).json({ error: 'Invalid user_id', stack: error });
