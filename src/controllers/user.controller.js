3bcf7928f70523acc03bec794c69eb9e341e5c09
Valery Retamoza <vretamoza@uninorte.edu.co>
2022-06-02 21:35:19 -0500

    Info de usuario terminado

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,18 @@
 const User = require('../models/User.model');
+const LikedPost = require('../models/LikedPost.model');
+const Post = require('../models/Post.model');
+const Follow = require('../models/Follow.model');
 
 async function UserInfo (req, res) {
     const { user_id } = req.query;
     try {
         const user = await User.findById(user_id).select('-password -birthdate');
-        res.json({ user });
+        const likedPosts = await LikedPost.countDocuments({user: user_id});
+        const userPosts = await Post.countDocuments({author: user_id});
+        const followers = await Follow.countDocuments({followed: user_id});
+        const following = await Follow.countDocuments({follower: user_id});
+
+        res.json({ ...user, likedPosts, userPosts, followers, following });
     } catch (error) {
         res.status(500).json({ error: 'Invalid user_id', stack: error });
     }
